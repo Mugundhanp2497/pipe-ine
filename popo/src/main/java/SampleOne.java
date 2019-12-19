32dbff6d24e40f1ce032b54b20f7c9d29b76e4a3
Mugundhan <58930635+Mugundhanp2497@users.noreply.github.com>
2019-12-19 10:33:01 +0530

    Add files via upload

---
+++ popo/src/main/java/SampleOne.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SampleOne
+{
+    public static void main(String[] args)
+    {
+       int a=5,b=5,c;
+       c=a*b;
+        System.out.println(c);
+        c=a+b;
+        System.out.println(c);
+
+    }
+}
